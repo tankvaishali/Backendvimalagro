7eff422788a67cef47bc7316e3c157344b7d87f0
tankvaishali <vaishalitank28603@gmail.com>
2025-08-26 12:09:25 +0530

    tls add in mongodb connection string

--- Mongodbdata/connection.js
+++ Mongodbdata/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const Connection = () => {
-    mongoose.connect("mongodb+srv://swadvimalgj:tbeLp4SVNcIvotnz@cluster0.kniraly.mongodb.net/vimalagro").then(() => {
+    mongoose.connect("mongodb+srv://swadvimalgj:tbeLp4SVNcIvotnz@cluster0.kniraly.mongodb.net/vimalagro",{ tls: true}).then(() => {
         console.log("mongoDB connected");
     })
 }
