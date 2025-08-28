be2e3d2117139554ba828b778b7bcdfaa54594d0
surbhi <surbhipansuriya633@gmail.com>
2025-08-28 17:28:06 +0530

    add product routes in code

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 import express from "express";
 import cors from "cors"
 import Connection from "./Mongodbdata/connection.js";
-import AddProduct from "./Product/Product.js";
+import counterApi from "./HomePage/Counter.js";
+import faq from "./AboutusPage/faq.js";
 import ViewMorebtn from "./Product/Button.js";
+import AddProduct from "./Product/Product.js";
 import productImageRoute from "./Product/productImageRoute.js";
 
 const app = express()
-
+app.use(express.json())
 app.use(cors())
 Connection()
-app.use(express.json());
 
-app.use("/view", ViewMorebtn); //for button hide and show 
+app.use("/view", ViewMorebtn);  // Button hide and show
+app.use("/counter", counterApi);
+app.use("/faq", faq);
 app.use("/api/products", AddProduct);
-app.use('/api/upload', productImageRoute); // Get Add Update and Delete Product
+app.use('/api/upload', productImageRoute);
 
 app.listen(8000, () => {
     console.log("server is running on localhost:8000");
